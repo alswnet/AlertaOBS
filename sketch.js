057df9710f4d26aaa3fea75db5acf6cc4d45d165
chepecarlos <chepecarlos@alswblog.org>
2022-03-02 22:53:43 -0600

    Mejorar algoritmo para mostar texto en pantalla del mensaje

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let NombreMQTT = "ChepeCarlos";
 let MensajeMQTT = "Amo a Lila <3 💕";
 let dibujarMensaje = false;
 let duracionMensaje = 6000;
-let inicioConteo = 0;
+let inicioConteo = -duracionMensaje;
 
 function setup() {
   colorFondo = color(0, 255, 0);
@@ -18,11 +18,44 @@ function setup() {
 function draw() {
   background(colorFondo);
   if (millis() - inicioConteo < duracionMensaje || dibujarMensaje) {
-    textSize(32);
-    fill(200);
-    rect(10, 30, 500, 100, 30);
-    fill(0);
-    text(`${NombreMQTT} `, 25, 60);
-    text(`${MensajeMQTT} `, 25, 110);
+    mostrarTexto(NombreMQTT, MensajeMQTT);
+    // textSize(32);
+    // fill(200);
+    // rect(10, 30, 500, 100, 30);
+    // fill(0);
+    // text(`${NombreMQTT} `, 25, 60, 400);
+    // text(`${MensajeMQTT} `, 25, 110, 400);
   }
+  // mostrarTexto("ChepeCarlos", "Hola Mundo");
+}
+
+function mostrarTexto(Nombre, Mensaje) {
+  let ColorTexto = color(200);
+  let ColorFondo = color(50);
+  let altoMensaje = 28;
+  let altoNombre = 20;
+  push();
+  translate(width / 2, height * 0.2);
+  rectMode(CENTER, CENTER);
+  textAlign(CENTER, CENTER);
+
+  textSize(altoMensaje);
+  let anchoMensaje = textWidth(Mensaje);
+
+  fill(ColorTexto);
+  rect(0, 0, anchoMensaje + 30, altoMensaje + 10, 5);
+  fill(ColorFondo);
+  text(Mensaje, 0, 0);
+
+  textSize(altoNombre);
+  let anchoNombre = textWidth(Nombre);
+  let xNombre = -anchoMensaje / 2 + anchoNombre / 2;
+  let yNombre = -20 - altoMensaje / 2 - altoNombre / 2;
+
+  fill(ColorTexto);
+  rect(xNombre, yNombre, anchoNombre + 30, altoNombre + 10, 5);
+  fill(ColorFondo);
+  text(Nombre, xNombre, yNombre);
+
+  pop();
 }
