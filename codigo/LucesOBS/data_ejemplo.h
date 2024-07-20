8b88162acd63f7962c4a8599dddba8d2605cef20
chepecarlos <chepecarlos@alsw.net>
2024-07-19 20:11:58 -0600

    Agregar codigo del pasado

---
+++ codigo/LucesOBS/data_ejemplo.h
@@ -0,0 +1,8 @@
+
+const char* ssid_1     = "123";
+const char* password_1 = "abc";
+const char* ssid_2     = "123";
+const char* password_2 = "abc";
+
+const char* BrokerMQTT = "192.168.50.200";
+const char* nombre = "Luces_OBS";
---
+++ codigo/LucesOBS/miRele.ino
@@ -0,0 +1,28 @@
+
+void configurarReles() {
+  for (int i = 0; i < CantidadLampara; i++) {
+    pinMode(PinLampara[i], OUTPUT);
+  }
+
+  actualizarRele();
+}
+
+void actualizarRele() {
+  for (int i = 0; i < CantidadLampara; i++) {
+    if (EstadosLampara[i] != EstadosLamparaAnterior[i]) {
+
+      Serial << "Rele " << i << ": " << (EstadosLampara[i] ? "encencido" : "apagado") << "\n";
+      TelnetStream << "Rele " << i << ": " << (EstadosLampara[i] ? "encencido" : "apagado") << "\n";
+
+      EstadosLamparaAnterior[i] = EstadosLampara[i];
+      digitalWrite(PinLampara[i], InvertidoLampara[i] ? !EstadosLampara[i]: EstadosLampara[i]);
+    }
+  }
+}
+
+void estadoRele() {
+  for (int i = 0; i < CantidadLampara; i++) {
+    Serial << "Rele " << i << ": " << (EstadosLampara[i] ? "encencido" : "apagado") << " - " << leerArchivo(i) << "\n";
+    TelnetStream << "Rele " << i << ": " << (EstadosLampara[i] ? "encencido" : "apagado") << " - " << leerArchivo(i) << "\n";
+  }
+}
